3c33ffd0b0381376853640ec01047bd21dbeb721
sabinopa <priasabino@gmail.com>
2023-09-05 18:20:41 -0300

    reorganiza metodos

--- ruby/password_validator.rb
+++ ruby/password_validator.rb
@@ -1,30 +1,27 @@
-def hello_message
+def print_hello_message
   puts "Hello! This is The Password Validator."
   puts "Depending on what you type your access will be diferent."
 end
 
-def email_register
-  puts "First, write your e-mail: "
-  email = gets.chomp
-end
-
-def password
+def get_password
   puts "Now, write your password:"
   password = gets.chomp
+end
 
+def run_validations
   if password.length > 8 
     puts "You are good making passwords, you got complete access!"
-  elsif password.length <= 7 || >= 5
+  elsif password.length(<= 7)  ||  password.length(>= 5)
     puts "Meh, your password is ok. You got a standard access."
   else password.length <= 4
     puts "You are not allowed to enter."
   end
 end
 
-def password_validator
-  hello_message
-  email_register  
-  password
+def validate_password
+  print_hello_message 
+  get_password
+  run_validations
 end
 
-password_validator
+validate_password
