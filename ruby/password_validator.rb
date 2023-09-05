30f6dcc77c3bf2092b05f679f2a7f65d8061d6e3
sabinopa <priasabino@gmail.com>
2023-09-05 19:40:06 -0300

    validacao letra maiuscula

--- ruby/password_validator.rb
+++ ruby/password_validator.rb
@@ -8,10 +8,13 @@ def get_password
 end
 
 def run_validations(password)
+  letters = /[a-zA-Z]/.match(password).to_a
   if password.length < 8
     message = "Must have at least 8 characteres."
+  elsif letters.none?{|char| char == char.upcase}
+    message = "Must have at least one capital letter."
   else
-    message = "Password length valid."
+    message = "Valid password."
   end
   message  
 end
