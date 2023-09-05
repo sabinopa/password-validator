ccff2e6c9a2e0fbe94fff3becfdeb6be34e933a8
sabinopa <priasabino@gmail.com>
2023-09-05 18:38:23 -0300

    valida quantidade de caracteres

--- ruby/password_validator.rb
+++ ruby/password_validator.rb
@@ -1,27 +1,27 @@
 def print_hello_message
   puts "Hello! This is The Password Validator."
-  puts "Depending on what you type your access will be diferent."
 end
 
 def get_password
   puts "Now, write your password:"
-  password = gets.chomp
+  gets.chomp
 end
 
-def run_validations
-  if password.length > 8 
-    puts "You are good making passwords, you got complete access!"
-  elsif password.length(<= 7)  ||  password.length(>= 5)
-    puts "Meh, your password is ok. You got a standard access."
-  else password.length <= 4
-    puts "You are not allowed to enter."
+def run_validations(password)
+  if password.length < 8
+    message = "Must have at least 8 characteres."
+  else
+    message = "Password length valid."
   end
+  message  
 end
 
 def validate_password
   print_hello_message 
-  get_password
-  run_validations
+  password = get_password
+  message = run_validations(password)
+  puts message
 end
 
 validate_password
+
